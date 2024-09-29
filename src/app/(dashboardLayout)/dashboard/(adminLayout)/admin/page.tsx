fc8eb7bba53ef066f1c198c2c690069e98839e95
aburahan8721 <aburaihan8721@gmail.com>
2024-09-29 17:52:06 +0600

    layout implementation done

---
+++ src/app/(dashboardLayout)/dashboard/(adminLayout)/admin/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdminHome = () => {
+  return <div>AdminHome page</div>;
+};
+
+export default AdminHome;
---
+++ src/app/(dashboardLayout)/dashboard/(userLayout)/user/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserHome = () => {
+  return <div>UserHome page</div>;
+};
+
+export default UserHome;
